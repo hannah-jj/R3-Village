808c21044d235630a8e32dc0334a572f9fc69b8b
Hui Jiang <huijiang@Huis-MBP.home>
2017-08-07 15:57:02 -0400

    user controller updated to render JSON for index, show, create& update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :update]
+
+	def index
+		@users = User.all
+		render json: @users
+	end
+
+	def show
+		render json: @user
+	end
+
+	def create
+		@user = User.create(user_params)
+		render json: @user
+	end
+
+	def update
+		@user.update(user_params)
+		render json: @user
+	end
+
+
+	private
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(
+			:name, :happiness, :pollution)
+	end
 end
