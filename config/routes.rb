5b1587d1aeece350ebb000ea09c7d73ddf47b0ef
Hui Jiang <huijiang@Huis-MBP.home>
2017-08-07 16:11:34 -0400

    routes updated for Box and User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :boxes
-  resources :users
+  resources :boxes, only: [:index, :show, :create, :update]
+  resources :users, only: [:index, :show, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
