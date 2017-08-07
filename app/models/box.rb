87041cbc12ed6316fb1b46339878135d3601b439
Hui Jiang <huijiang@Huis-MBP.home>
2017-08-07 16:03:34 -0400

    Box model update with default values and validation

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,4 +1,14 @@
 class Box < ApplicationRecord
 	belongs_to :user
 	belongs_to :item
+
+	before_save :default_values
+	validates :user_id, presence: true
+	validates :item_id, presence: true
+
+	def default_values
+		self.reuse ||= 0
+		self.recycled ||= false
+		self.trashed ||= false
+	end
 end
