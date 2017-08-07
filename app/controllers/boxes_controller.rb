494a848a4c7f994ee03fb124a5b7399e153ca302
Hui Jiang <huijiang@Huis-MBP.home>
2017-08-07 16:07:56 -0400

    Box controller updated for API

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,2 +1,33 @@
 class BoxesController < ApplicationController
+	before_action :set_box, only: [:show, :update]
+
+	def index
+		@boxes = Box.all
+		render json: @boxes
+	end
+
+	def show
+		render json: @box
+	end
+
+	def create
+		@box = Box.create(box_params)
+		render json: @box
+	end
+
+	def update
+		@box.update(box_params)
+		render json: @box
+	end
+
+
+	private
+	def set_box
+		@box = Box.find(params[:id])
+	end
+
+	def box_params
+		params.require(:box).permit(
+			:name, :happiness, :pollution)
+	end
 end
