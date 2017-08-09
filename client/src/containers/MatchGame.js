c458cd24f35859c6d3988524bdce0a915ccae087
Hui Jiang <huijiang@Huis-MBP.home>
2017-08-09 16:31:01 -0400

    match card game functional without checking winnings

--- client/src/containers/MatchGame.js
+++ client/src/containers/MatchGame.js
@@ -6,21 +6,101 @@ import * as actions from '../actions/index';
 
 
 class MatchGame extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			win: false,
+			flipped: [false, false, false, false, false,false, false, false,false, false, false, false],
+			firstClick: {status: false, card: "", index: 10}
+		};
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
 	componentDidMount(){
 		this.props.actions.fetchItems('/api/items');
 	}
+
+	componentDidUpdate(prevProps, prevState) {
+
+	}
+
+	updateFlippedArray = (array, index, new_item) => {
+		var new_array = array;
+		new_array[index] = new_item;
+		this.setState({flipped: new_array});
+	}
+
+	resetFirstClick = () => {
+		console.log("resetting first Click");
+		this.setState({firstClick: {status: false, card: "", index: 10}}, function () {
+    		console.log("resetFirstClick"); console.log(this.state)});
+	}
+
+	setFirstClick = (card_name, index) => {
+		this.setState({firstClick: {status: true, card: card_name, index: index}}, function () {
+    		console.log("setFirstclick"); console.log(this.state)});
+	}
+
+	checkWin = () => {
+		
+	}
+
+	handleClick = event => {
+		console.log("before")
+		console.log(this.state);
+		var t = event.target;
+		var clicked = t.getAttribute('data-key');
+		var card_name = t.alt;
+
+		if (this.state.flipped[clicked] === false ) { // if not already flipped go into action
+			console.log("flip over");
+			this.updateFlippedArray(this.state.flipped, clicked, true);
+
+			if (this.state.firstClick.status === false){ //first click
+				this.setFirstClick(card_name, clicked);
+			} 
+			else { //if firstClick already happened
+				if (this.state.firstClick.card === card_name){
+					// do some animation later
+					console.log("matched!")
+					this.resetFirstClick();
+				}
+				else {
+					console.log("no match should cover them")
+					console.log(`1st click is ${this.state.firstClick.index} 2nd is ${clicked}`)
+					this.resetFirstClick();
+					setTimeout(this.updateFlippedArray(this.state.flipped, clicked, false),2000);
+					setTimeout(this.updateFlippedArray(this.state.flipped, this.state.firstClick.index, false),2000);
+
+				}
+			}
+		}// end of main if
+
+		console.log("after")
+		console.log(this.state);
+	}
+
+
+
 	render(){
-		const renderGames = this.props.games.map((gamePiece, index) => 
-			<td key={index} onClick={this.handleClick}>
-				<img src={gamePiece.picture} alt={gamePiece.name} />
-			</td>
+		const renderGames = this.props.games.map((gamePiece, index) => {
+			if (this.state.flipped[index] === false) {
+			return (<div key={index} style={{display: 'inline-block', padding: 2}}>
+				<img data-key={index} onClick={this.handleClick} src='/defaults/default.png' alt={gamePiece.name} />
+			</div>)
+		} else {
+			return (<div key={index} onClick={this.handleClick} style={{display: 'inline-block', padding: 2}}>
+				<img data-key={index} src={gamePiece.picture} alt={gamePiece.name} />
+			</div>)
+		}
+
+}
 		);
 
 		return (
-			<div>
-				<table>
-					<tbody><tr>{renderGames}</tr></tbody>
-				</table>
+			<div style={{width: 820, height: 620, backgroundColor: 'powderblue'}} >
+				{renderGames}
 			</div>
 		);
 	}
